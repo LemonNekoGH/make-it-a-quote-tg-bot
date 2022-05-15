48c9ac10eaede14685017562a2b737280ae6a4c6
LemonNeko <chheese048@gmail.com>
2022-05-15 15:11:36 +0800

    feat: 已经可以正常的合成图片了

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-// import Jimp from 'jimp'
+import Jimp from 'jimp'
 // import path from 'path'
-import { generate } from 'text-to-image'
-import { writeFile } from 'fs/promises'
+import { UltimateTextToImage, VerticalImage } from 'ultimate-text-to-image'
 
 /**
  * 把任意头像、id、文字转成一张图片
@@ -9,9 +8,18 @@ import { writeFile } from 'fs/promises'
  * @param id 会在前面加上一个破折号
  * @param text 图片正文
  */
-// @ts-expect-error
-const makeItAQuote = (avatar: ArrayBuffer, id: string, text: string): ArrayBuffer => {
-  return Buffer.from('')
+export const makeItAQuote = async (avatarIn: Buffer, maskIn: Buffer, idIn: string, textIn: string): Promise<Jimp> => {
+  const avatar = await Jimp.read(avatarIn)
+  const text = await Jimp.read(await genTextWithIdPic(idIn, textIn))
+  const mask = await Jimp.read(maskIn)
+  // 把头像大小缩放到文本的高度
+  avatar.resize(text.getHeight(), text.getHeight())
+  // 把遮罩缩放到文本的高度
+  mask.resize(text.getHeight(), text.getHeight())
+  // 生成一张宽度是头像和文本宽度之和，高度是文本高度的黑色图片
+  const bg = await Jimp.create(text.getHeight() + text.getWidth(), text.getHeight(), 'black')
+  // 把头像叠在左边，把文本叠在右边
+  return bg.composite(avatar, 0, 0).composite(text, text.getHeight(), 0).composite(mask, 0, 0)
 }
 
 /**
@@ -20,25 +28,23 @@ const makeItAQuote = (avatar: ArrayBuffer, id: string, text: string): ArrayBuffe
  * @param text 图片正文
  */
 // @ts-expect-error
-const genTextWithIdPic = async (id: string, text: string): ArrayBuffer => {
-  // const pic = new Jimp(1200, 630, 0x000000ff)
-  // const font = await Jimp.loadFont(path.resolve(__dirname, './assets/Alibaba-PuHuiTi-Regular.ttf'))
-  // pic.print(font, 20, 20, `
-  //   ${text}
-  //   —— @${id}
-  // `)
-  // await pic.write('test.png')
-
-  const dataUri = await generate(`
-  ${text}
-  —— @${id}
-`, {
-    bgColor: 'black',
-    textColor: 'white',
-    textAlign: 'right'
+export const genTextWithIdPic = async (id: string, text: string): Buffer => {
+  const image = new VerticalImage([
+    new UltimateTextToImage(text, {
+      maxWidth: 500,
+      fontSize: 32,
+      fontColor: 0xffffff,
+      lineHeight: 36
+    }), // 正文部分
+    new UltimateTextToImage('@' + id, {
+      fontSize: 20,
+      fontColor: 0xffffff99,
+      align: 'right',
+      width: 500,
+      marginTop: 32
+    })
+  ], {
+    margin: 32
   })
-  const toWrite = Buffer.from(dataUri.split(',')[1], 'base64') // 转成 buffer 以便输出到图片
-  await writeFile('test.png', toWrite)
+  return image.render().toBuffer()
 }
-
-genTextWithIdPic('中文测试', 'test')
---
+++ src/test.ts
@@ -0,0 +1,15 @@
+import { readFile } from 'fs/promises'
+import { makeItAQuote } from './main'
+
+const main = async (): Promise<void> => {
+  const avatarBuffer = await readFile('./src/assets/default_profile.png')
+  const maskBuffer = await readFile('./src/assets/gradient-mask.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test1.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test2.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test3.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test4.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test5.png')
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main()
