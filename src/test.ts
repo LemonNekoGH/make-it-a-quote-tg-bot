8dbb244ac0c3c8aa9f08f3752fc0c5a6dc73d1cd
LemonNeko <chheese048@gmail.com>
2022-06-02 22:24:06 +0800

    test: 更新测试代码

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,16 @@
 import { readFile } from 'fs/promises'
-import { makeItAQuote } from './utils'
+import { getArgsFromMessageText, makeItAQuote } from './utils'
 
 const main = async (): Promise<void> => {
   const avatarBuffer = await readFile('./src/assets/default_profile.png')
   const maskBuffer = await readFile('./src/assets/gradient-mask.png')
-  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test1.png')
-  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test2.png')
-  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test3.png')
-  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test4.png')
-  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试')).writeAsync('pngTest/test5.png')
+  // 放一个空的参数
+  const args = getArgsFromMessageText('')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试', args)).writeAsync('pngTest/test1.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试', args)).writeAsync('pngTest/test2.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试', args)).writeAsync('pngTest/test3.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试', args)).writeAsync('pngTest/test4.png')
+  await (await makeItAQuote(avatarBuffer, maskBuffer, '测试测试', '测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试，测试测试测试测试测试测试测试测试', args)).writeAsync('pngTest/test5.png')
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
